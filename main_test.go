fb2f586dcb3a748f5c13a595f51d7e3cf5912631
YoloZoloo <zolozooro@gmail.com>
2022-11-16 21:41:08 +0900

    add CI pipeline add go test

---
+++ main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"os"
+	"testing"
+	. "time"
+)
+
+func TestCurlMainScreen(t *testing.T) {
+	go main()
+	Sleep(3 * Second)
+	requestURL := "http://localhost:9999"
+	res, err := http.Get(requestURL)
+	if err != nil {
+		t.Errorf("error: %q", err)
+	}
+	//must return 405
+	if res.StatusCode != 405 {
+		t.Errorf("resp status: %d", res.StatusCode)
+	}
+}
+
+func TestLoginTest(t *testing.T) {
+	requestURL := "http://localhost:9999/api/login"
+	user_id := os.Getenv("LOGIN_TEST_USER_ID")
+	pass := os.Getenv("LOGIN_TEST_USER_PASS")
+	creds := &userCredentials{UserID: user_id, Password: pass}
+	t.Log("data: ", creds)
+	data, _ := json.Marshal(creds)
+	t.Log("data: ", data)
+	// t.Log("data: ", []byte(data))
+	res, err := http.Post(requestURL, "application/json", bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		t.Errorf("error: %q", err)
+	}
+	if res.StatusCode != 200 {
+		t.Errorf("resp status: %d", res.StatusCode)
+	}
+}
